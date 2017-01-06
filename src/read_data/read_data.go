10504b856c4a1856fa97eb123331f7d879b0b029
cosiek <witold.majchrzak@21s.pl>
2017-01-06 22:23:55 +0100

    #2: create stops from data provaded in a file

--- src/read_data/read_data.go
+++ src/read_data/read_data.go
@@ -3,9 +3,12 @@ package read_data
 import (
     "fmt"
     "log"
+    "strconv"
     "strings"
 
     "third_party/ods"
+
+    "structs"
 )
 
 func Read_data() {
@@ -20,7 +23,26 @@ func Read_data() {
         log.Fatal(err)
     }
 
+    var t string
+    stops := make([]structs.Stop, 0)
     for _, row := range table {
-        fmt.Println(strings.Join(row, "|"))
+        if row[0] == "Stops" || row[0] == "Lines"{
+            fmt.Println(row[0])
+            t = row[0]
+        } else if row[0] == "Id"{
+            continue
+        } else {
+            if t == "Stops"{
+                id, _ := strconv.Atoi(row[0])
+                x, _ := strconv.Atoi(row[2])
+                y, _ := strconv.Atoi(row[3])
+                stop := structs.Stop{id, row[1], x, y}
+                stops = append(stops, stop)
+                fmt.Println(stop)
+            } else {
+                fmt.Println(strings.Join(row, "|"))
+            }
+        }
     }
+    fmt.Println(stops)
 }
