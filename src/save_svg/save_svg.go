392e49e06a9a70278849c2688b8b039dcbb72ce8
cosiek <witold.majchrzak@21s.pl>
2017-01-08 22:52:01 +0100

    #3: add first draft version of output svg generating

--- src/save_svg/save_svg.go
+++ src/save_svg/save_svg.go
@@ -2,17 +2,46 @@ package save_svg
 
 import (
     "fmt"
-    "io/ioutil"
+    "os"
 
+    "third_party/svgo"
+
+    "helpers"
     "structs"
 )
 
+var STOP_ICON_RANGE int = 5
+
 func Save(stops map[int]structs.Stop, lines []structs.Line){
-    d1 := []byte("Hello World")
-    err := ioutil.WriteFile("export.svg", d1, 0644)
-    if err != nil{
-        panic(err)
-    } else {
-        fmt.Println("saved.")
+    // start new canvas
+    f, err := os.Create("export.svg")
+    if err != nil { panic(err) }
+    defer f.Close()
+    canvas := svg.New(f)
+    // calculate canvas width and height
+    max_x := 0
+    max_y := 0
+    for _, stop := range stops{
+        max_x = simple_math.Max(max_x, stop.X)
+        max_y = simple_math.Max(max_y, stop.Y)
     }
+
+    canvas.Start(max_x, max_y)
+    canvas.Grid(0, 0, max_x, max_y, 10, "stroke:black;opacity:0.05")
+    for _, line := range lines{
+        x_es := make([]int, 0)
+        y_s := make([]int, 0)
+        for _, stop := range line.Stops{
+            x_es = append(x_es, stop.X)
+            y_s = append(y_s, stop.Y)
+        }
+        canvas.Polyline(x_es, y_s, "stroke:black")
+    }
+
+    for _, stop := range stops{
+        canvas.Circle(stop.X, stop.Y, STOP_ICON_RANGE)
+    }
+    canvas.End()
+
+    fmt.Println("saved.")
 }
